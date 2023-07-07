e5fc0cce38681a425855390df3a4877f47d8c264
unknown <jhon.churivanti@tecsup.edu.pe>
2023-07-07 11:43:36 -0500

    Se agrega el proyecto

---
+++ MODULO 2/Ejemplo1.py
@@ -0,0 +1,48 @@
+lives = 2
+
+#print(f"Lives: ", lives)
+
+lives = lives - 1
+
+#print(f"Lives:" , lives)
+
+lives = lives - 1
+
+#print(f"Lives: ", lives)
+#print("GAME OVER!")
+
+
+#SEGUNDO DE EJEMPLO // MOSTRANDO LISTA DE AMIGOS
+
+amigos = "Mis Amigos:"
+
+amigos = amigos + " Frank,"
+amigos = amigos + " Jhon,"
+amigos = amigos + " Karli"
+
+#print(amigos)
+# 
+# // TERCER EJEMPLO / BILLTERA ACTUALIZANDO
+# 
+
+cuenta = 500
+
+#print("Deposito: S/ 300.00")
+
+cuenta = cuenta + 300
+
+#print("COMPRA: S/ 39.00")
+
+cuenta = cuenta - 39
+
+#print("COMPRA FINAL / DIA: S/ 65.00")
+
+cuenta-=65
+
+#print(f"TOTAL SOLES / CUENTA : ", cuenta)
+
+# //// AGREGANDO FUNCIÓN DE FACTORIAL
+
+
+
+
